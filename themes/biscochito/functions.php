606635d9affe6671109ac56eb8e89e138b2c7560
Nayeli Jordan <nayeli.jordan16@gmail.com>
2018-04-12 16:42:11 -0500

    first commit

---
+++ themes/biscochito/functions.php
@@ -0,0 +1,187 @@
+<?php 
+
+/**
+* Define paths to javascript, styles, theme and site.
+**/
+define( 'JSPATH', get_stylesheet_directory_uri() . '/js/' );
+define( 'THEMEPATH', get_stylesheet_directory_uri() . '/' );
+define( 'SITEURL', get_site_url() . '/' );
+
+
+/*------------------------------------*\
+	#SNIPPETS
+\*------------------------------------*/
+//require_once( 'inc/pages.php' );
+// require_once( 'inc/pages.php' );
+// require_once( 'inc/post-types.php' );
+// require_once( 'inc/taxonomies.php' );
+
+/*------------------------------------*\
+	#GENERAL FUNCTIONS
+\*------------------------------------*/
+
+/**
+* Enqueue frontend scripts and styles
+**/
+add_action( 'wp_enqueue_scripts', function(){
+ 
+	wp_enqueue_script( 'jquery', 'https://code.jquery.com/jquery-3.2.1.min.js', array(''), '2.1.1', true );
+	wp_enqueue_script( 'materialize_js', JSPATH.'bin/materialize.min.js', array('jquery'), '1.0', true );
+	//wp_enqueue_script( 'wow_js', JSPATH.'wow.min.js', array(), '', true );
+	wp_enqueue_script( 'dw_functions', JSPATH.'functions.js', array('materialize_js'), '1.0', true );
+ 
+	wp_localize_script( 'dw_functions', 'siteUrl', SITEURL );
+	wp_localize_script( 'dw_functions', 'theme_path', THEMEPATH );
+	
+	wp_localize_script( 'dw_functions', 'isHome', (string)is_front_page() );
+	// wp_localize_script( 'dw_functions', 'isSingular', (string)is_singular() );
+	
+	// $is_home = is_front_page() ? "1" : "0";
+	// wp_localize_script( 'ri_functions', 'isHome', $is_home );
+	// $is_singular = is_singular() ? "1" : "0";
+	// wp_localize_script( 'ri_functions', 'isSingular', $is_singular );
+	// $is_archive = is_archive() ? "1" : "0";
+	// wp_localize_script( 'ri_functions', 'isArchive', $is_archive );
+	// $is_author = is_author() ? "1" : "0";
+	// wp_localize_script( 'ri_functions', 'isAuthor', $is_author );
+ 
+});
+
+/**
+* Configuraciones WP
+*/
+
+//Habilitar thumbnail en post
+add_theme_support( 'post-thumbnails' ); 
+
+//Habilitar menú (aparece en personalizar)
+add_action('after_setup_theme', 'add_top_menu');
+function add_top_menu(){
+	register_nav_menu('top_menu',__('Top menu'));
+}
+
+//Delimitar número palabras excerpt
+function custom_excerpt_length( $length ) {
+	return 15;
+}
+add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
+
+
+/**
+* Optimización
+*/
+
+// REMOVE WP EMOJI
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+
+/**
+* SEO y Analitics
+**/
+
+//Código Analitics
+function add_google_analytics() {
+    echo '<script src="https://www.google-analytics.com/ga.js" type="text/javascript"></script>';
+    echo '<script type="text/javascript">';
+    echo 'var pageTracker = _gat._getTracker("UA-117075138-1");';
+    echo 'pageTracker._trackPageview();';
+    echo '</script>';
+}
+add_action('wp_footer', 'add_google_analytics');
+
+/* Aplaza el análisis de JavaScript para una carga más rápida */
+if(!is_admin()) {
+    // Move all JS from header to footer
+    remove_action('wp_head', 'wp_print_scripts');
+    remove_action('wp_head', 'wp_print_head_scripts', 9);
+    remove_action('wp_head', 'wp_enqueue_scripts', 1);
+    add_action('wp_footer', 'wp_print_scripts', 5);
+    add_action('wp_footer', 'wp_enqueue_scripts', 5);
+    add_action('wp_footer', 'wp_print_head_scripts', 5);
+}
+
+/**
+ * Custom walker class.
+ */
+class WPDocs_Walker_Nav_Menu extends Walker_Nav_Menu {
+ 
+    /**
+     * Starts the list before the elements are added.
+     *
+     * Adds classes to the unordered list sub-menus.
+     *
+     * @param string $output Passed by reference. Used to append additional content.
+     * @param int    $depth  Depth of menu item. Used for padding.
+     * @param array  $args   An array of arguments. @see wp_nav_menu()
+     */
+    function start_lvl( &$output, $depth = 0, $args = array() ) {
+        // Depth-dependent classes.
+        $indent = ( $depth > 0  ? str_repeat( "\t", $depth ) : '' ); // code indent
+        $display_depth = ( $depth + 1); // because it counts the first submenu as 0
+        $classes = array(
+            'sub-menu',
+            ( $display_depth % 2  ? 'menu-odd' : 'menu-even' ),
+            ( $display_depth >=2 ? 'sub-sub-menu' : '' ),
+            'menu-depth-' . $display_depth
+        );
+        $class_names = implode( ' ', $classes );
+ 
+        // Build HTML for output.
+        $output .= "\n" . $indent . '<ul class="' . $class_names . '">' . "\n";
+    }
+ 
+    /**
+     * Start the element output.
+     *
+     * Adds main/sub-classes to the list items and links.
+     *
+     * @param string $output Passed by reference. Used to append additional content.
+     * @param object $item   Menu item data object.
+     * @param int    $depth  Depth of menu item. Used for padding.
+     * @param array  $args   An array of arguments. @see wp_nav_menu()
+     * @param int    $id     Current item ID.
+     */
+    function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+        global $wp_query;
+        $indent = ( $depth > 0 ? str_repeat( "\t", $depth ) : '' ); // code indent
+ 
+        // Depth-dependent classes.
+        $depth_classes = array(
+            ( $depth == 0 ? 'main-menu-item' : 'sub-menu-item' ),
+            ( $depth >=2 ? 'sub-sub-menu-item' : '' ),
+            ( $depth % 2 ? 'menu-item-odd' : 'menu-item-even' ),
+            'menu-item-depth-' . $depth
+        );
+        $depth_class_names = esc_attr( implode( ' ', $depth_classes ) );
+ 
+        // Passed classes.
+        $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+        $class_names = esc_attr( implode( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) ) );
+ 
+        // Build HTML.
+        $output .= $indent . '<li id="nav-menu-item-'. $item->ID . '" class="' . $depth_class_names . ' ' . $class_names . '">';
+ 
+        // Link attributes.
+        $attributes  = ! empty( $item->attr_title ) ? ' id="'  . esc_attr( $item->attr_title ) .'"' : '';
+        $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
+        $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
+        $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
+        $attributes .= ' class="' . ( $depth > 0 ? 'sub-menu-link' : 'main-menu-link' ) . '"';
+ 
+        // Build HTML output and pass through the proper filter.
+        $item_output = sprintf( '%1$s<a%2$s>%3$s%4$s%5$s</a>%6$s',
+            $args->before,
+            $attributes,
+            $args->link_before,
+            apply_filters( 'the_title', $item->title, $item->ID ),
+            $args->link_after,
+            $args->after
+        );
+        $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+    }
+}
+
---
+++ themes/biscochito/js/functions.js
@@ -0,0 +1,46 @@
+var $=jQuery.noConflict();
+ 
+(function($){
+	"use strict";
+	$(function(){
+ 
+		/*------------------------------------*\
+			#GLOBAL
+		\*------------------------------------*/
+
+		$(document).ready(function() {
+			footerBottom();
+		});
+ 
+		$(window).on('resize', function(){
+
+			
+		});
+ 
+		$(document).scroll(function() {
+
+
+		});
+ 
+		if( parseInt( isHome ) ){
+			$(document).ready(function() {
+
+			});
+		} 
+ 
+	});
+})(jQuery);
+ 
+/**
+ * Fija el footer abajo
+ */
+function footerBottom(){
+	var alturaFooter = getFooterHeight();
+	$('.main-body').css('padding-bottom', alturaFooter );
+}
+function getHeaderHeight(){
+	return $('.js-header').outerHeight();
+}// getHeaderHeight
+function getFooterHeight(){
+	return $('footer').outerHeight();
+}// getFooterHeight
